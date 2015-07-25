3eeecddfc90c1457eeb14f33190fa31fcbcbcf42
David Grayson <davidegrayson@gmail.com>
2015-07-25 12:41:48 -0700

    Added a basic C test just to make sure the header can compile.

--- test/build.sh
+++ test/build.sh
@@ -6,5 +6,8 @@ then
 fi
 echo "intsafe version: $VER"
 
-g++ --std=gnu++11 -I../$VER test.cpp -o run_test
+echo compiling C tests
+gcc -I../$VER test.c -o run_test_c
 
+echo compiling C++ tests
+g++ --std=gnu++11 -I../$VER test.cpp -o run_test
---
+++ test/test.c
@@ -0,0 +1,6 @@
+#include <intsafe.h>
+
+int main()
+{
+    return 0;
+}
