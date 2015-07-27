845999856ab61e269168fd06a2e0875dac6ac556
David Grayson <davidegrayson@gmail.com>
2015-07-26 22:13:01 -0700

    Fixed grepcheck.sh to only look for function names that have boundaries on both sides.

--- test/grepcheck.sh
+++ test/grepcheck.sh
@@ -1,7 +1,7 @@
 # Script for checking an implementation of intsafe.h to make sure it
 # at least has the functions specified in Microsoft's documentation.
 
-cd `dirname $0`/..
+cd `dirname $0`
 
 if [ -z "$VER" ]
 then
@@ -10,17 +10,20 @@ fi
 echo "intsafe version: $VER"
 
 count=0
-FUNC_NAMES=($(<test/function_names.txt))
+FUNC_NAMES=($(<function_names.txt))
+echo > missing_functions.txt
 for func in "${FUNC_NAMES[@]}"
 do
     if [ -z "$func" ]
     then
         continue
     fi
-    (grep -r $func $VER 2>&1 > /dev/null)
+    func="${func//$'\r'}"
+    (grep -e "\b${func}\b" -r ../$VER 2>&1 > /dev/null)
     if [ $? -ne 0 ]
     then
         count=$((count + 1))
         echo "$count: missing $func"
+        echo $func >> missing_functions.txt
     fi
 done
