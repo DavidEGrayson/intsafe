49f5a420a6443177073543ae6b58d0a82b038300
David Grayson <davidegrayson@gmail.com>
2015-07-26 17:32:38 -0700

    generate.rb: Added the generate_types method.

--- test/generate.rb
+++ test/generate.rb
@@ -45,31 +45,35 @@ class CNumberType < Struct.new(:name, :camel_name, :type_id)
   end
 end
 
-char_type = -1
-pointer_size = 8
-Types64 = [
-  CNumberType['UCHAR', 'UChar', 1],
-  CNumberType['BYTE', 'Byte', 1],
-  CNumberType['CHAR', 'Char', char_type],
-  CNumberType['USHORT', 'UShort', 2],
-  CNumberType['WORD', 'Word', 2],
-  CNumberType['SHORT', 'Short', -2],
-  CNumberType['UINT', 'UInt', 4],
-  CNumberType['ULONG', 'ULong', 4],
-  CNumberType['DWORD', 'DWord', 4],
-  CNumberType['INT', 'Int', -4],
-  CNumberType['LONG', 'Long', -4],
-  CNumberType['ULONGLONG', 'ULongLong', 8],
-  CNumberType['INT64', 'Int64', -8],
-  CNumberType['UINT_PTR', 'UIntPtr', pointer_size],
-  CNumberType['size_t', 'SizeT', pointer_size],
-  CNumberType['DWORD_PTR', 'DWordPtr', pointer_size],
-  CNumberType['ULONG_PTR', 'ULongPtr', pointer_size],
-  CNumberType['INT_PTR', 'IntPtr', -pointer_size],
-  CNumberType['LONG_PTR', 'LongPtr', -pointer_size],
-  CNumberType['ptrdiff_t', 'PtrdiffT', -pointer_size],
-  CNumberType['SSIZE_T', 'SSIZET', -pointer_size],
-]
+def generate_types(pointer_size, char_signed)
+  char_type = char_signed ? -1 : 1
+  pointer_size = 8
+  [
+    CNumberType['UCHAR', 'UChar', 1],
+    CNumberType['BYTE', 'Byte', 1],
+    CNumberType['CHAR', 'Char', char_type],
+    CNumberType['USHORT', 'UShort', 2],
+    CNumberType['WORD', 'Word', 2],
+    CNumberType['SHORT', 'Short', -2],
+    CNumberType['UINT', 'UInt', 4],
+    CNumberType['ULONG', 'ULong', 4],
+    CNumberType['DWORD', 'DWord', 4],
+    CNumberType['INT', 'Int', -4],
+    CNumberType['LONG', 'Long', -4],
+    CNumberType['ULONGLONG', 'ULongLong', 8],
+    CNumberType['INT64', 'Int64', -8],
+    CNumberType['UINT_PTR', 'UIntPtr', pointer_size],
+    CNumberType['size_t', 'SizeT', pointer_size],
+    CNumberType['DWORD_PTR', 'DWordPtr', pointer_size],
+    CNumberType['ULONG_PTR', 'ULongPtr', pointer_size],
+    CNumberType['INT_PTR', 'IntPtr', -pointer_size],
+    CNumberType['LONG_PTR', 'LongPtr', -pointer_size],
+    CNumberType['ptrdiff_t', 'PtrdiffT', -pointer_size],
+    CNumberType['SSIZE_T', 'SSIZET', -pointer_size],
+  ]
+end
+
+Types64 = generate_types(8, true)
 
 Indent = "    "
 
