11bb275cc9062bdf2ec4fd8f5daea25b13bf503c
David Grayson <davidegrayson@gmail.com>
2015-08-01 19:48:08 -0700

    generated: Copied the license from mingw-w64's intsafe.h.

--- generated/top.h
+++ generated/top.h
@@ -1,4 +1,9 @@
-/* This file was auto-generated from https://github.com/DavidEGrayson/intsafe
+/**
+ * This file has no copyright assigned and is placed in the Public Domain.
+ * This file is part of the mingw-w64 runtime package.
+ * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ *
+ * This file was auto-generated from https://github.com/DavidEGrayson/intsafe
  *
  * This file is an implementation of Microsoft's intsafe.h header, which
  * provides inline functions for safe integer conversions and math operations:
